53a7a9ca259a36365be449443cc2d93cfc78f18b
Roman Belik <roman.belik@gmail.com>
2018-06-04 21:45:27 +0300

    add second commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,23 @@
+class ProductsController < ApplicationController
+	helper_method :sort_column, :sort_direction
+	def index
+    @products = Product.includes(:category).order(sort_column + " " + sort_direction).page(params[:page]).per(50)
+  end
+
+	def show
+    @product = Product.find(params[:id])
+  end
+ 
+  def new
+  end
+
+  private
+  
+  def sort_column
+    Product.includes(:category).include?(params[:sort]) ? params[:sort] : "name"
+  end
+  
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+end
