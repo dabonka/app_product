81f6d8ba5196bc2e8bcf21200bb1b088bd36cab0
Roman Belik <roman.belik@gmail.com>
2018-06-06 22:41:43 +0300

    fix blanks

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-
   it "has a valid factory" do
-  	category = create(:category)
+    category = create(:category)
     expect(category).to be_valid
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,9 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-
   it "has a valid factory" do
-  	product = create(:product)
+    product = create(:product)
     expect(product).to be_valid
   end
 end
