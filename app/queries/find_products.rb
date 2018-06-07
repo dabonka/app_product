90f7442a89630b223e71637cfc647b31b8a1b0d1
Roman Belik <roman.belik@gmail.com>
2018-06-07 22:32:52 +0300

    add queries

--- app/queries/find_products.rb
+++ app/queries/find_products.rb
@@ -6,41 +6,50 @@ class FindProducts
   end
 
   def call(params)
-    scoped = search(initial_scope, params[:search])
-    scoped = filter_by_price(scoped, params[:from_price], params[:to_price])
-    scoped = filter_by_properties(scoped, parmas[:properties])
-    scoped = filter_by_category(scoped, params[:category_id])
-    scoped = sort(scoped, params[:sort_type], params[:sort_direction]
-    scoped = paginate(scoped, params[:page]
+    scoped = initial_scope
+    scoped = search(scoped, params[:product_name])
+    #scoped = filter_by_price(scoped, params[:price_from], params[:price_to])
+    scoped = price_from(scoped, params[:price_from])
+    scoped = price_to(scoped, params[:price_to])
+    scoped = filter_by_category(scoped, params[:category])
     scoped
   end
 
   private def search(scoped, query = nil)
-    query ? scoped.where("title ILIKE '%?%'", query) : scoped
+    (query && query.length > 0) ? scoped.where("name LIKE ?", "%#{query}%") : scoped
   end
 
-  private def filter_by_price(scoped, from = nil, to = nil)
-    from ? scoped.where('price > ?', from_price) : scoped
-    to ? scoped.where('price < ?', to_price) : scoped
+  # private def filter_by_price(scoped, price_from = nil, price_to = nil)
+  #   price_from ? scoped.where('price > ?', price_from.to_i) : scoped
+  #   price_to ? scoped.where('price < ?', price_to.to_i) : scoped
+  # end
+
+  private def price_from(scoped, price_from = nil)
+    (price_from && price_from.length > 0) ? scoped.where('price > ?', price_from.to_i) : scoped
   end
 
-  private def filter_by_properties(scoped, properties = nil)
-    if properties
-      scoped.joins(:product_properties).where(property_id: properties)
-    else
-      scoped
-    end 
-  end 
-
-  private def filter_by_category(scoped, category_id = nil)
-    category_id ? scoped.where(category_id: category_id) : scoped
+  private def price_to(scoped, price_to = nil)
+    (price_to && price_to.length > 0) ? scoped.where('price < ?', price_to.to_i) : scoped
   end
 
-  private def sort(scoped, sort_type = :desc, sort_direction = :price) 
-    scoped.order(sort_type => sort_direction)
+
+  # private def filter_by_properties(scoped, properties = nil)
+  #   if properties
+  #     scoped.joins(:product_properties).where(property_id: properties)
+  #   else
+  #     scoped
+  #   end 
+  # end 
+
+  private def filter_by_category(scoped, category = nil)
+    (category && category.length > 0)  ? scoped.where(category: category) : scoped
   end
 
-  private def paginate(scoped, page_number = 0)
-    scoped.page(page_number)
-  end
+  # private def sort(scoped, sort_type = :desc, sort_direction = :price) 
+  #   scoped.order(sort_type => sort_direction)
+  # end
+
+  # private def paginate(scoped, page_number = 0)
+  #   scoped.page(page_number)
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   resources :products
   resources :categories
 
+  root :to => "products#index"
+
   get '/' => "products#index"
+  post '/' => "products#index"
 end
